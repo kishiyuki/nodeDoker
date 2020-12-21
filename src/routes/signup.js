27273da30a9d01bda5783a106065a56ece62a306
岸本　悠希 <kishimoto@a.local>
2020-12-21 17:09:36 +0900

    progress1221ver2

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// res.render('auth/signup', {});
+	res.redirect('http://localhost/signin');
 });
 
 
