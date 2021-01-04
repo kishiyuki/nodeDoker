949824c24a6700f344f8f437d7547dc29b9fcec6
岸本　悠希 <kishimoto@a.local>
2021-01-04 19:54:50 +0900

    progress0104ver3

--- src/routes/evaluate.js
+++ src/routes/evaluate.js
@@ -109,22 +109,22 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
   async function getESR(){
     const users = await query('select id,user_name,email,profession from users;');
     const events = await query('select * from events where id = ' + req.body.event_id + ';');
-    if (req.user.email) {
-      for (i = 0; i < users.length; i++) {
-        if(req.user.email == users[i].email){
-          id = users[i].id;
-          profession = users[i].profession;
-        }
-      }
-    }
-    // if (req.body.email) {
+    // if (req.user.email) {
     //   for (i = 0; i < users.length; i++) {
-    //     if(req.body.email == users[i].email){
+    //     if(req.user.email == users[i].email){
     //       id = users[i].id;
     //       profession = users[i].profession;
     //     }
     //   }
     // }
+    if (req.body.email) {
+      for (i = 0; i < users.length; i++) {
+        if(req.body.email == users[i].email){
+          id = users[i].id;
+          profession = users[i].profession;
+        }
+      }
+    }
     if(profession == "student"){
       const events_students_con = await query('select * from events_students where event_id = ' + req.body.event_id + ' and student_id = ' + id +';');
       if(events_students_con.length != 0){
@@ -137,19 +137,9 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
       }
     }
     if(bool){
-      if(users.length != 0){
-        for (i = 0; i < users.length; i++) {
-          if(req.user.email == users[i].email){
-            sender = users[i];
-          }
-          if(req.body.receiver_id == users[i].id){
-            receiver = users[i];
-          }
-        }
-      }
       // if(users.length != 0){
       //   for (i = 0; i < users.length; i++) {
-      //     if(req.body.email == users[i].email){
+      //     if(req.user.email == users[i].email){
       //       sender = users[i];
       //     }
       //     if(req.body.receiver_id == users[i].id){
@@ -157,6 +147,16 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
       //     }
       //   }
       // }
+      if(users.length != 0){
+        for (i = 0; i < users.length; i++) {
+          if(req.body.email == users[i].email){
+            sender = users[i];
+          }
+          if(req.body.receiver_id == users[i].id){
+            receiver = users[i];
+          }
+        }
+      }
       eventlist = events;
       obj = {
         eventlist:eventlist,
@@ -175,22 +175,22 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
   }
   async function getSR(){
     const users = await query('select id,user_name,email,profession from users;');
-    if (req.user.email) {
-      for (i = 0; i < users.length; i++) {
-        if(req.user.email == users[i].email){
-          id = users[i].id;
-          profession = users[i].profession;
-        }
-      }
-    }
-    // if (req.body.email) {
+    // if (req.user.email) {
     //   for (i = 0; i < users.length; i++) {
-    //     if(req.body.email == users[i].email){
+    //     if(req.user.email == users[i].email){
     //       id = users[i].id;
     //       profession = users[i].profession;
     //     }
     //   }
     // }
+    if (req.body.email) {
+      for (i = 0; i < users.length; i++) {
+        if(req.body.email == users[i].email){
+          id = users[i].id;
+          profession = users[i].profession;
+        }
+      }
+    }
     if(profession == "student"){
       const events_students_con = await query('select * from events_students where event_id = ' + req.body.event_id + ' and student_id = ' + id +';');
       if(events_students_con != 0){
@@ -204,18 +204,18 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
     }
     if(users.length != 0){
       for (i = 0; i < users.length; i++) {
-        if(req.user.email == users[i].email){
-          sender = users[i];
-        }
-        if(req.body.receiver_id == users[i].id){
-          receiver = users[i];
-        }
-        // if(req.body.email == users[i].email){
+        // if(req.user.email == users[i].email){
         //   sender = users[i];
         // }
         // if(req.body.receiver_id == users[i].id){
         //   receiver = users[i];
         // }
+        if(req.body.email == users[i].email){
+          sender = users[i];
+        }
+        if(req.body.receiver_id == users[i].id){
+          receiver = users[i];
+        }
       }
     }
   }
@@ -223,7 +223,7 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
   let dtx;
   let handler;
   async function destroy(){
-    if(bool){
+    // if(bool){
       address = req.body.event_id.toString() + "_" + sender.id.toString() + "_" + req.body.receiver_id.toString();
       const evaluates = await query('select * from evaluates where event_id = ' + req.body.event_id.toString() + ' and sender_id = ' + sender.id.toString() + ' and receiver_id = ' + req.body.receiver_id.toString() + ';');
       if(evaluates.length != 0){
@@ -247,7 +247,7 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
           }
         });
       }
-    }
+    // }
   }
   let now = getStringFromDate(new Date());
   let freeStatement = "";
@@ -257,7 +257,7 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
   var txEtime;
   var txStime
   async function add(){
-    if(bool){
+    // if(bool){
       if(req.body.free != null){
         for(var i = 0; i<req.body.free.length; i++){
           freeStatement = freeStatement + req.body.free[i].toString();
@@ -311,7 +311,7 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
           console.log(err);
         }
       });
-    }
+    // }
   }
   async function total(){
     await getSR();
@@ -323,7 +323,7 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
     }
     res.json(obj);
   }
-  if(req.user){
+  // if(req.user){
     if (!errors.isEmpty()) {
       getESR();
     }else {
@@ -340,13 +340,13 @@ router.post('/', [body("action").not().isEmpty().withMessage("アクションを
       // }
       // console.log("alltime: " + (endTime - startTime));
     }
-  } else {
-    console.log("sign inへ")
-    obj = {
-      status:401
-    }
-    res.json(obj);
-  }
+  // } else {
+  //   console.log("sign inへ")
+  //   obj = {
+  //     status:401
+  //   }
+  //   res.json(obj);
+  // }
 });
 
 function getStringFromDate(date) {
