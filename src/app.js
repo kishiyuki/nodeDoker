5ca21570891374cffd579a3d5e441a3910ab435a
岸本　悠希 <kishimoto@a.local>
2021-01-13 16:39:09 +0900

    progress0113updatemysql

--- src/app.js
+++ src/app.js
@@ -39,8 +39,8 @@ let saiyoueConfirmationRouter = require('./routes/saiyoueConfirmation');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
